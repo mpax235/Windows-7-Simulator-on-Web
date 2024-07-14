3fd5074ffed89046afa2e6f74c62f5a681524b67
mpax235 <millerpgraves@gmail.com>
2024-07-14 11:18:59 -0700

    Update date.js

--- js/date.js
+++ js/date.js
@@ -1,1 +1,15 @@
+function updateDateTime() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1; // Months are zero-based
+    var day = now.getDate();
+    var dateString = month + '/' + day + '/' + year;
 
+    var dateTimeString = dateString;
+
+    var datetimeElement = document.getElementById('date');
+    datetimeElement.innerHTML = dateString;
+}
+
+// Update every second (1000 milliseconds)
+setInterval(updateDateTime, 1000);
